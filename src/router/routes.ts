4f4ba526f1579126a96bb6a6a38930eed1e12302
Hecate2 <2474101468@qq.com>
2024-04-15 13:17:33 +0800

    change name to 灸疗仪

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -3,7 +3,7 @@ import OrgView from '../views/Orgs/OrgsView.vue'
 import LoginView from '../views/Login/LoginView.vue'
 
 const routes = [
-    {path: '/aijiumachines', name: '艾灸机', component: AijiuMachinesView},    
+    {path: '/aijiumachines', name: '灸疗仪', component: AijiuMachinesView},    
     {path: '/orgs', name: '组织', component: OrgView},
     {path: '/login', name: '登录', component: LoginView}
 ]
